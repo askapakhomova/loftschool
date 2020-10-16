7b5cfab0247779401df37e9563f2a1facfc9ecb8
Asiia Pakhomova <askapakhomova@gmail.com>
2020-10-16 19:15:01 +0300

    Изменение в задании 6

--- task_6.php
+++ task_6.php
@@ -3,10 +3,21 @@
 echo "<table border='1'>";
 for ($row=1; $row <= 10; $row++) {
     for ($col=1; $col <= 10; $col++) {
-        $a = "$row * $col";
-        echo "<td>$a</td> ";
+        if (($col%2) == 0 && ($row%2) == 0) {
+            $a = "$row * $col";
+            echo "<td> ($a) </td> ";
+        } elseif (($col%2) != 0 && ($row%2) != 0){
+            $a = "$row * $col";
+            echo "<td> [$a] </td> ";
+        } else {
+            $a = $row * $col;
+            echo "<td> $a </td> ";
+        }
     }
     echo "</tr>";
 }
 echo "</table>";
+
+
+
 
